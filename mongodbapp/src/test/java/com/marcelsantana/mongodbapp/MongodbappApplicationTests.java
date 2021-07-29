bb8c3a907ab8390a9c18135475fdd23c46f57edd
Marcel Santana <marceltuk@gmail.com>
2021-07-29 15:04:24 -0300

    updated gitignore file

---
+++ mongodbapp/src/test/java/com/marcelsantana/mongodbapp/MongodbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelsantana.mongodbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
